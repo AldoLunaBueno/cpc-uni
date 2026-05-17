f002beedd91b9153cd6f2549b84f7b5d8b6d581b
AldoLunaBueno <albanovaphi7@gmail.com>
2026-05-17 17:09:03 -0500

    criptografía

--- 00_repasar_cpp/rand_gen_blum_blum_shub.cpp
+++ 00_repasar_cpp/rand_gen_blum_blum_shub.cpp
@@ -5,8 +5,11 @@
 using namespace std;
 
 int main() {
-    int n_bits = 1000000;
-    long long p = 998551, q = 997307;
+    // int n_bits = 1000000;
+    int n_bits = 10;
+    // long long p = 998551, q = 997307;
+    long long p = 11, q = 19;
+
     if (p % 4 != 3) {
         cout << "p no cumplen resto 3 módulo 4";
         return -1;
@@ -18,7 +21,9 @@ int main() {
     random_device rnd;
     mt19937 gen(rnd());
     uniform_int_distribution<> distr(1, 1000000);
-    long long s = distr(gen);
+    // long long s = distr(gen);
+    long long s = 5;
+    
     long long m = p*q;
     long long x = s*s % m; // x0 (no se toma su bit)
     ofstream myFile("output.txt");
